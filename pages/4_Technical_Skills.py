f478cca26f1f8776276ccbbab63ce005c5089b99
Shobhit <shobhit.singh@iitg.ac.in>
2024-05-26 10:11:52 +0530

    Technical Skill Final

--- pages/4_Technical_Skills.py
+++ pages/4_Technical_Skills.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils.get_footer import get_footer
 
 def display_skill_progress(skill, completion, custom_line=""):
     """Displays a progress bar for a specific skill with a friendly message and custom line."""
@@ -23,70 +24,71 @@ def main():
 
     skills = {
         "Languages": {
-            "C/C++": (1.0, "I opted for C++ as a subject during my secondary school education, resulting in a proficient understanding of the language."),
-            "Python": (1.0, "In my master's thesis, I used Python for data analysis and machine learning."),
-            "SQL": (0.8, "I have DBMS in my course curriculum, which has provided me with a comprehensive understanding of SQL."),
-            "MATLAB": (0.5, "I have used MATLAB for my academic assignments."),
+            "C/C++": (1.0, "Developed a proficient understanding of C++ during my secondary school education as part of my curriculum."),
+            "Python": (1.0, "Utilized Python extensively for data analysis and machine learning in my master's thesis."),
+            "SQL": (0.8, "Gained comprehensive understanding of SQL through coursework in Database Management Systems."),
+            "MATLAB": (0.5, "Applied MATLAB in various academic assignments and projects."),
         },
 
         "Deployment": {
-            "Machine Learning": (0.9, "I have learned and deployed multiple machine learning models in a couple of projects."),
-            "Artificial Neural Networks": (0.85, "I compared the performance of ANNs with other machine learning models and presented a case study on a crime rate dataset."),
-            "Convolutional Neural Networks": (0.8, "I have worked on a skin cancer detection and classification model using CNNs like VGG16 and ResNet50."),
-            "Recurrent Neural Networks": (0.5, "I have a theoretical understanding of RNNs and their applications."),
-            "Natural Language Processing": (0.75, "I have worked on sentiment analysis and text classification using NLP."),
-            "Large Language Models": (0.7, "I have worked on a question-answering system using BERT and fine-tuned it on the SQuAD dataset."),
+            "Machine Learning": (0.9, "Deployed multiple machine learning models in various projects, demonstrating practical proficiency."),
+            "Artificial Neural Networks": (0.85, "Presented a case study on crime rate prediction, comparing ANNs with other machine learning models."),
+            "Convolutional Neural Networks": (0.8, "Developed a skin cancer detection and classification model using CNN architectures such as VGG16 and ResNet50."),
+            "Recurrent Neural Networks": (0.5, "Acquired a theoretical understanding of RNNs and their applications."),
+            "Natural Language Processing": (0.8, "Executed sentiment analysis and text classification projects using NLP techniques."),
+            "Large Language Models": (0.7, "Implemented a question-answering system using BERT, fine-tuning it on the SQuAD dataset."),
         },
 
         "Frameworks": {
-            "Streamlit": (1.0, "I built an end-to-end data analysis and machine learning web application using Streamlit."),
-            "Tkinter": (0.8, "I created a GUI for a project to communicate with Arduino and generate sensor data."),
-            "FastAPI": (0.7, "I have created a simple API using FastAPI."),
-            "LangChain": (0.6, "I have created a simple chatbot using LangChain."),
+            "Streamlit": (1.0, "Built a comprehensive data analysis and machine learning web application using Streamlit."),
+            "Tkinter": (0.8, "Developed a GUI for a project to interface with Arduino and display sensor data."),
+            "FastAPI": (0.7, "Created a simple and efficient API using FastAPI."),
+            "LangChain": (0.6, "Developed a basic chatbot leveraging LangChain framework."),
         },
 
         "Databases": {
-            "MySQL": (0.9, "I have created multiple databases and tables in MySQL."),
-            "MongoDB": (0.7, "I have created a simple database in MongoDB."),
+            "MySQL": (0.9, "Designed and managed multiple databases and tables in MySQL."),
+            "MongoDB": (0.7, "Created and managed a basic database using MongoDB."),
         },
 
         "MLOps": {
-            "Git": (0.9, "I have used Git for version control in multiple projects."),
-            "Docker": (0.8, "I have containerized my machine learning models using Docker."),
-            "Kubernetes": (0.7, "I have deployed my machine learning models on Kubernetes."),
-            "MLflow": (0.7, "I have used MLflow for tracking and managing machine learning experiments."),
-            "TensorBoard": (0.7, "I have used TensorBoard for visualizing machine learning models."),
-            "DVC": (0.6, "I have used DVC for versioning and managing machine learning models."),
+            "Git": (0.8, "Utilized Git for version control across numerous projects."),
+            "Docker": (0.8, "Containerized machine learning models using Docker for deployment."),
+            "Kubernetes": (0.7, "Deployed machine learning models using Kubernetes for scalable solutions."),
+            "MLflow": (0.7, "Employed MLflow for tracking and managing machine learning experiments."),
+            "TensorBoard": (0.7, "Used TensorBoard for visualizing the performance of machine learning models."),
+            "DVC": (0.8, "Applied DVC for version control and management of machine learning models."),
         },
         
         "Libraries": {
-            "Pandas": (0.9, "I used Pandas extensively for data manipulation and analysis in my various data science projects."),
-            "NumPy": (0.9, "I utilized NumPy for numerical computing in numerous machine learning and data analysis tasks."),
-            "Matplotlib": (0.8, "I created various static visualizations for academic reports and data presentations using Matplotlib."),
-            "Seaborn": (0.8, "I built statistical graphics on top of Matplotlib for data visualization projects."),
-            "Plotly": (0.8, "I developed interactive visualizations for web applications and dashboards using Plotly."),
-            "Scikit-learn": (0.9, "I applied machine learning algorithms using Scikit-learn in multiple projects, including my master's thesis."),
-            "TensorFlow": (0.9, "I implemented deep learning models using TensorFlow for several projects, such as image classification and NLP."),
-            "NLTK": (0.7, "I processed and analyzed text data using NLTK for a sentiment analysis project."),
-            "spaCy": (0.7, "I used spaCy for natural language processing tasks in a text classification project."),
-            "Hugging Face Transformers": (0.7, "I worked on NLP tasks using Hugging Face Transformers, including fine-tuning models for question-answering systems and chatbots."),
+            "Pandas": (0.9, "Extensively used Pandas for data manipulation and analysis in various data science projects."),
+            "NumPy": (0.9, "Leveraged NumPy for numerical computing in machine learning and data analysis tasks."),
+            "Matplotlib": (0.8, "Generated static visualizations for academic reports and data presentations using Matplotlib."),
+            "Seaborn": (0.8, "Created statistical graphics for data visualization projects using Seaborn."),
+            "Plotly": (0.8, "Developed interactive visualizations for web applications and dashboards using Plotly."),
+            "Scikit-learn": (0.9, "Implemented machine learning algorithms using Scikit-learn in multiple projects, including my master's thesis."),
+            "TensorFlow": (0.9, "Built deep learning models with TensorFlow for projects like image classification and NLP."),
+            "NLTK": (0.7, "Processed and analyzed text data for a sentiment analysis project using NLTK."),
+            "spaCy": (0.7, "Used spaCy for various NLP tasks in a text classification project."),
+            "Hugging Face Transformers": (0.7, "Fine-tuned models for NLP tasks, including question-answering systems and chatbots, using Hugging Face Transformers."),
         },
 
         "Dashboarding": {
-            "Tableau": (0.8, "I created interactive dashboards for data visualization in my internship at a health data analytics company."),
-            "Power BI": (0.7, "I used Power BI to develop analytics dashboards for a project on inventory control system in my company."),
+            "Tableau": (0.8, "Designed interactive dashboards for data visualization during my internship at a health data analytics company."),
+            "Power BI": (0.7, "Developed analytics dashboards for an inventory control system project using Power BI."),
         },
 
         "Miscellaneous": {
-            "Data Structures and Algorithms": (0.9, "I have a strong foundation in data structures and algorithms from my coursework and competitive programming."),
-            "Object-Oriented Programming": (0.9, "I applied OOP principles in various projects, including automation project and machine learning model implementation."),
-            "Functional Programming": (0.8, "I gained experience with functional programming paradigms during my coursework and through implementing algorithms in Python."),
-            "Software Design Patterns": (0.8, "I am familiar with common design patterns and have applied them in software development projects."),
-            "RESTful APIs": (0.8, "I built APIs conforming to REST architecture for web applications and machine learning model deployment."),
-            "Web Scraping": (0.8, "I extracted data from websites using Python for a project on market research and data collection."),
-            "Regular Expressions": (0.7, "I used regular expressions for pattern matching and text data extraction in multiple projects."),
+            "Data Structures and Algorithms": (0.9, "Established a strong foundation in data structures and algorithms through coursework and competitive programming."),
+            "Object-Oriented Programming": (0.9, "Applied OOP principles in various projects, including automation and machine learning model implementation."),
+            "Functional Programming": (0.8, "Gained experience in functional programming paradigms through coursework and algorithm implementation in Python."),
+            "Software Design Patterns": (0.8, "Applied common design patterns in software development projects."),
+            "RESTful APIs": (0.8, "Developed APIs adhering to REST architecture for web applications and machine learning model deployment."),
+            "Web Scraping": (0.8, "Conducted data extraction from websites using Python for market research and data collection projects."),
+            "Regular Expressions": (0.7, "Utilized regular expressions for pattern matching and text data extraction in multiple projects."),
         }
     }
+
 
     for category_title, skill_dict in skills.items():
         st.header(category_title)
@@ -109,3 +111,5 @@ if __name__ == "__main__":
     )
 
     main()
+    footer = get_footer()
+    st.markdown(footer, unsafe_allow_html=True)
