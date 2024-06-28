da41d62f65199ece20f30b30346679c101981056
Shobhit <shobhit.singh@iitg.ac.in>
2024-06-28 23:09:53 +0530

    all done 2

--- Home.py
+++ Home.py
@@ -7,11 +7,9 @@ def lottie(path: str):
     with open(path) as f:
         return json.load(f)
 
-
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
 
 def main():
     load_css("utils/home.css")
@@ -23,11 +21,11 @@ def main():
         st.header("About Me:")
         st.write("""
         Reflecting on my journey, taking that leap of faith from mechanical engineering into the vibrant worlds of Artificial Intelligence and data analysis was one of the most pivotal decisions of my life. The path has been nothing short of rewarding. Delving into these domains, I find myself continuously invigorated by the challenges and opportunities to apply my skills in innovative ways. My enthusiasm for AI has only grown, and as a Senior Executive Data Scientist, I take pride in my accomplishments.
-        
+
         I've made a substantial impact in medical device manufacturing and clinical diagnostics by leveraging technology effectively. With my expertise in machine learning and advanced data analysis, coupled with a master's degree from IIT Guwahati, I'm enthusiastic about furthering innovation and excellence in the field.
-        
+
         Beyond data analysis, I enjoy strength training, sketching, swimming, and capturing the beauty of the world through photography. I'm passionate about making a positive difference and look forward to embracing new opportunities for growth and impact! I thrive in collaborative environments where I can apply my skills to solve complex challenges and deliver impactful solutions.
-        
+
         Feel free to explore my portfolio and get in touch if you're as passionate about data and innovation as I am. Let's connect and create something amazing together! I'm seeking opportunities to contribute my data science and artificial intelligence expertise to drive forward-thinking initiatives.
         """)
         
@@ -40,7 +38,6 @@ def main():
     
     col1, col2 = st.columns(2)
     with col1:
-        # write in the center of the column
         st.markdown("<h4 style='text-align: center; color: #ff6347;'>Navigation</h4>", unsafe_allow_html=True)
         st.info(
             "Use the dropdown on the top left to navigate between pages. All the contact information is available in the footer section."
@@ -50,7 +47,6 @@ def main():
         st.info(
             "This app is maintained by Shobhit Kumar Singh. You can learn more about me on my [LinkedIn](https://www.linkedin.com/in/shobhit-singhh/) or You can reach out to me via [email](mailto:shobhit22iit@gmail.com)."
         )
-
 
 def sidebar():
     # in sidebar i want to add my photo 
@@ -87,12 +83,9 @@ def sidebar():
     )
 
 
-
 if __name__ == "__main__":
     st.set_page_config(layout="wide", page_title="My Portfolio", page_icon="🌟")
     main()
     sidebar()
     footer = get_footer()
     st.markdown(footer, unsafe_allow_html=True)
-
-    
