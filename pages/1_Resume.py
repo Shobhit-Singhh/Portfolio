78a759e293a03057e810cd2a4b01bb44fd83438c
Shobhit-Singhh <117563572+Shobhit-Singhh@users.noreply.github.com>
2025-02-11 15:04:52 +0530

    Update 1_Resume.py

--- pages/1_Resume.py
+++ pages/1_Resume.py
@@ -30,7 +30,7 @@ def main():
 
 def sidebar():
     # in sidebar i want to add my photo 
-    st.sidebar.image("data/images/shobhit.jpg", use_column_width=True)
+    st.sidebar.image("data/images/shobhit.jpg", use_container_width=True)
     st.markdown(
         """
         <style>
